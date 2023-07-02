aa4c8c34659e97ef7aef67d506c76a118eb7c16a
Greenteami <103634448+Greenteami@users.noreply.github.com>
2023-07-02 22:27:34 +0800

    app.js

--- app.js
+++ app.js
@@ -1,36 +1,25 @@
-// 引入所需的模組
 const express = require('express');
-
-// 建立 Express 應用程式
 const app = express();
 const port = 3000;
-const exphbs = require('express-handlebars')
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set ('view engine', 'handlebars')
+app.use(express.static('public'));
 
-// 設定首頁路由
 app.get('/', (req, res) => {
-  res.send('<h1>首頁</h1>');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
-// 設定 About 路由
 app.get('/about', (req, res) => {
-  res.send('<h1>About</h1>');
+  res.sendFile(__dirname + '/public/about.html');
 });
 
-// 設定 Portfolio 路由
 app.get('/portfolio', (req, res) => {
-  res.send('<h1>Portfolio</h1>');
+  res.sendFile(__dirname + '/public/portfolio.html');
 });
 
-// 設定 Contact 路由
 app.get('/contact', (req, res) => {
-  res.send('<h1>Contact</h1>');
+  res.sendFile(__dirname + '/public/contact.html');
 });
 
-// 監聽應用程式
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
-
